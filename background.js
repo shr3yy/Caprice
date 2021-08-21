e2b92bcf1d02886bb2fba47b99d3197599a630b0
Shreyansh Malviya <80682098+shr3yy@users.noreply.github.com>
2021-08-21 17:20:25 +0530

    Add files via upload

--- background.js
+++ background.js
@@ -1,20 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
     localStorage.clear();
     localStorage.setItem("caprice_status", "true")
+    localStorage.setItem('theme', 'dark')
 })
 
 chrome.runtime.onMessage.addListener((response, sender, sendResponse) => {
     console.log(response);
-    if(response.message === "status"){
+    if (response.message == "open") {
+        window.open("index.html")
+    }else if(response.message === "status"){
         sendResponse({status : localStorage.getItem("caprice_status")});
         return true;
     }else{
-        var temp = localStorage.getItem(res.url);
+        var temp = localStorage.getItem(response.url);
+        console.log(temp)
         if (temp) {
-            var existing = localStorage.getItem(res.url, res.todo);
-            localStorage.setItem(res.url, existing + res.todo);
+            var existing = localStorage.getItem(response.url, response.todo);
+            localStorage.setItem(response.url, existing + response.todo);
         }else{
-            localStorage.setItem(res.url, res.todo);
+            localStorage.setItem(response.url, response.todo);
         }
     }
 })
--- popup.js
+++ popup.js
@@ -7,44 +7,13 @@ if(localStatus === "false"){
 }
 
 
-
-
 toggle.addEventListener("change", () => {
     if(toggle.checked){
         localStorage.setItem("caprice_status", "false");
         console.log(toggle.checked)
+        chrome.runtime.sendMessage({message : "hide"})
     }else{
         localStorage.setItem("caprice_status", "true");
         console.log(toggle.checked)
     }
 })
-
-
-
-
-// document.querySelector("h3").innerText = localStorage.getItem("caprice_status")
-
-// const temp = document.createElement("input")
-// temp.type = "checkbox"
-// temp.id = "toggle"
-// console.log(typeof(localStorage.getItem("caprice_status")))
-
-// if(localStorage.getItem("caprice_status") === 'false'){
-//     temp.checked = true
-// }else{
-//     console.log("matlab local true")
-//     temp.checked = false
-// }
-
-// console.log(temp.checked)
-
-
-// const lbl = document.querySelector("label")
-// lbl.appendChild(temp)
-// console.log(lbl.innerHTML)
-// lbl.innerHTML = lbl.innerHTML + '<span class="slider round"></span>'
-
-
-// const toggle = document.querySelector("#toggle");
-// toggle.checked = !(localStorage.getItem("caprice_status"))
-
